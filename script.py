42fdd16c55a780a9dd02c2ad51ccd0e84a145b98
Andres <a301540@uach.mx>
2018-09-11 20:53:13 -0600

    Agregado de nombres

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ class MainHandler(tornado.web.RequestHandler):
     @gen.engine
     def get(self):
         self.write("Yendo a dormir...")
-        
+
         yield gen.Task(IOLoop.instance().add_timeout, time.time() + 1)
         self.write("Estoy despierto!")
         self.finish()
@@ -24,3 +24,7 @@ if __name__ == "__main__":
     app = make_app()
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
+
+# Andres Ivan Hernandez Zamora
+# Aaron Jafed Martinez Sarmiento
+# Raul Issack Ramirez Gonzalez
