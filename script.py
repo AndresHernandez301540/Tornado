ba1c1d01ec253cfbcdb7767a517e0d897c21ce9e
Andres <a301540@uach.mx>
2018-09-11 20:46:54 -0600

    Primer commit

---
+++ script.py
@@ -0,0 +1,26 @@
+import time
+import tornado.web
+from tornado.ioloop import IOLoop
+from tornado import gen
+
+
+class MainHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    @gen.engine
+    def get(self):
+        self.write("Yendo a dormir...")
+        
+        yield gen.Task(IOLoop.instance().add_timeout, time.time() + 1)
+        self.write("Estoy despierto!")
+        self.finish()
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
